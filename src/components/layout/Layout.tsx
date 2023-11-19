5c20732711e8f8113cb592fb72adfb969ba20cf9
TigranAvushyan <tigran.avushyan@gmail.com>
2023-11-19 15:32:07 +0400

    fix

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -44,6 +44,33 @@ export const Layout: FC<Props> = ({}) => {
     <div className={styles.layout}>
       <img src={grapes} className={styles.image} />
 
+      <div className={styles.container}>
+        <NavLink
+          to='/map'
+          className={({ isActive }) => `${isActive && styles.active}`}
+        >
+          Интерактивная карта
+        </NavLink>
+        <NavLink
+          to='/list'
+          className={({ isActive }) => `${isActive && styles.active}`}
+        >
+          Список заболеваний
+        </NavLink>
+        <NavLink
+          to='/charts'
+          className={({ isActive }) => `${isActive && styles.active}`}
+        >
+          Графики
+        </NavLink>
+        <NavLink
+          to='/archive'
+          className={({ isActive }) => `${isActive && styles.active}`}
+        >
+          Архив
+        </NavLink>
+      </div>
+
       <div className={styles.headerDropdown}>
         <h1 className={styles.header}>
           Аналитика по:{' '}
